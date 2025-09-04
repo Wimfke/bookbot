f401b204bfcbbef5ceea8ed6c8745dd4fd3c0d12
Wimfke <somers.wim@gmail.com>
2025-09-04 22:37:49 +0200

    modified stats.py

--- stats.py
+++ stats.py
@@ -2,12 +2,10 @@ def count_words(text):
     return len(text.split())
 
 def num_char(text):
-    words = (text.lower()).split()
     char_dict = {}
-    for word in words:
-        for char in word:
-            if not char in char_dict:
-                char_dict[char] = 1
-            else:
-                char_dict[char] += 1
+    for char in text.lower():
+        if not char in char_dict:
+            char_dict[char] = 1
+        else:
+            char_dict[char] += 1
     return char_dict
