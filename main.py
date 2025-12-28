5aed7feb0dacf74c3e6f17edf38c1f7c4644d470
Wimfke <somers.wim@gmail.com>
2025-12-28 19:57:35 +0100

    CH3 L1: report

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from stats import count_words, count_characters
+from stats import count_words, count_characters, sort_dict
 
 def get_book_text(filepath):
     with open(filepath) as f:
@@ -10,8 +10,16 @@ def main():
     text = get_book_text(file_location)
     num_words = count_words(text)
     chars_dict = count_characters(text)
+    sorted_list = sort_dict(chars_dict)
+
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {file_location}")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    for char in chars_dict:
-        print(f"'{char}': {chars_dict[char]}")
+    print("--------- Character Count -------")
+    for char in sorted_list:
+        if char["char"].isalpha():
+            print(f"{char["char"]}: {char["num"]}")
+    print("============= END ===============")
 
 main()
--- stats.py
+++ stats.py
@@ -9,3 +9,17 @@ def count_characters(text):
         else:
             characters[char] = 1
     return characters
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dict(char_dict):
+    dict_list = []
+    for char in char_dict:
+        new_dict = {}
+        new_dict["char"] = char
+        new_dict["num"] = char_dict[char]
+        dict_list.append(new_dict)
+        
+    dict_list.sort(reverse=True, key=sort_on)
+    return dict_list
