397290c354fd86e442b0b4a0cea10f2a19ae36e4
Wimfke <somers.wim@gmail.com>
2025-12-28 18:37:45 +0100

    L4: Count words

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print("greetings boots")
+def get_book_text(filepath):
+    with open(filepath) as f:
+        file_contents = f.read()
+    return file_contents
+
+def count_words(text):
+    return len(text.split())
+
+def main():
+    file_location = "books/frankenstein.txt"
+    text = get_book_text(file_location)
+    num_words = count_words(text)
+    print(f"Found {num_words} total words")
+
+main()
